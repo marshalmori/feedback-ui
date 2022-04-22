6aadbf90e97973d4a6fbf3852ef76ad1e45b488b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-04-22 16:49:54 -0300

    Create a real time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,8 +5,21 @@ import Button from "./shared/Button";
 
 function FeedbackForm(props) {
   const [text, setText] = useState("");
+  const [btnDisable, setBtnDisable] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisable(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length < 10) {
+      setMessage("Text must be at least 10 characteres");
+      setBtnDisable(true);
+    } else {
+      setMessage(null);
+      setBtnDisable(false);
+    }
+
     setText(e.target.value);
   };
 
@@ -22,10 +35,12 @@ function FeedbackForm(props) {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit" version="secondary">
+          <Button type="submit" isDisabled={btnDisable} version="secondary">
             Send
           </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );

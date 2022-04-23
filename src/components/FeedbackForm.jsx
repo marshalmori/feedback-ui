950c0d7d3a771656197ab8c69a4dfecbc079ef2e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-04-23 19:12:11 -0300

    Create add feedback

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,7 +5,7 @@ import Card from "./shared/Card";
 import Button from "./shared/Button";
 import RatingSelect from "./RatingSelect";
 
-function FeedbackForm(props) {
+function FeedbackForm({ handleAdd }) {
   const [text, setText] = useState("");
   const [rating, setRating] = useState(10);
   const [btnDisable, setBtnDisable] = useState(true);
@@ -26,9 +26,25 @@ function FeedbackForm(props) {
     setText(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        //pode ser escrito da forma curta
+        //text,
+        //rating,
+        text: text,
+        rating: rating,
+      };
+
+      handleAdd(newFeedback);
+      setText("");
+    }
+  };
+
   return (
     <Card>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
         <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">

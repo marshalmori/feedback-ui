ee3cd1631b6961d14e991ec99fe08b3187e3775a
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-04-23 22:38:07 -0300

    Create Link to About Page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -46,6 +47,7 @@ function App() {
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
+      <AboutIconLink />
     </Router>
   );
 }
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
+
 import Card from "../components/shared/Card";
 
 function AboutPage() {
   return (
     <Card>
       <div className="about"></div>
+      <h1>About This Project</h1>
       <p>This is a React app to leave feedback for a product or service</p>
       <p>Version: 1.0.0</p>
       <p>
-        <a href="/">Back To Home</a>
+        <Link to="/">Back To Home</Link>
       </p>
     </Card>
   );

d6d3b1e671dc9e19bf03868b468970f2239f6cff
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-04-25 15:04:32 -0300

    Create side effects with useEffect

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 
 import Card from "./shared/Card";
@@ -12,7 +12,15 @@ function FeedbackForm() {
   const [btnDisable, setBtnDisable] = useState(true);
   const [message, setMessage] = useState("");
 
-  const { addFeedback } = useContext(FeedbackContext);
+  const { addFeedback, feedbackEdit } = useContext(FeedbackContext);
+
+  useEffect(() => {
+    if (feedbackEdit.edit === true) {
+      setBtnDisable(false);
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
+    }
+  }, [feedbackEdit]);
 
   const handleTextChange = (e) => {
     if (text === "") {
